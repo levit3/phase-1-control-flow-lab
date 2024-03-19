72b30eee3682daf3d891c7f7d4609040c0169d96
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-19 09:39:56 +0300

    adds functions and loops

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let ticket;
+  if (ride <= 400) {
+    ticket = "This one is on me!"
+    return ticket
+  }
+  else if (ride >200, ride <2000) {
+    ticket = "That will be twenty bucks."
+    return ticket
+  }
+  else if (ride > 2000, ride < 2500) {
+    ticket = "I will gladly take your thirty bucks."
+    return ticket
+  }
+  else {
+    ticket = "No can do."
+    return ticket
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break
+    case "not as generous" :
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+  }
 }
